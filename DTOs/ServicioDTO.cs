096e8c5276bdc1ee1315b8df343fa6da6b9f919b
Matias Alicata <matiasalicata@gmail.com>
2023-09-17 18:32:42 -0300

    Agregar operaciones CRUD en Servicio

--- DTOs/ServicioDTO.cs
+++ DTOs/ServicioDTO.cs
@@ -1,6 +1,11 @@
-﻿namespace TechOil.DTOs
+﻿using TechOil.Entities;
+
+namespace TechOil.DTOs
 {
     public class ServicioDTO
     {
+        public string Descr { get; set; }
+        public EstadoServicio EstadoServicio { get; set; }
+        public Decimal ValorHora { get; set; }
     }
 }
--- DataAccess/Repositories/ServicioRepository.cs
+++ DataAccess/Repositories/ServicioRepository.cs
@@ -1,4 +1,5 @@
-﻿using TechOil.DataAccess.Repositories.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using TechOil.DataAccess.Repositories.Interfaces;
 using TechOil.Entities;
 
 namespace TechOil.DataAccess.Repositories
@@ -8,6 +9,31 @@ namespace TechOil.DataAccess.Repositories
         public ServicioRepository(ApplicationDbContext context) : base(context)
         {
             
+        }
+
+        public override async Task<bool> Update(Servicio updateServicio)
+        {
+            var servicio = await _context.Servicios.FirstOrDefaultAsync(x => x.CodServicio == updateServicio.CodServicio);
+
+            if (servicio == null) { return false; }
+
+            servicio.Descr = updateServicio.Descr;
+            servicio.EstadoServicio = updateServicio.EstadoServicio;
+            servicio.ValorHora = updateServicio.ValorHora;
+            
+            _context.Servicios.Update(servicio);
+            return true;
+        }
+
+        public override async Task<bool> Delete(int id)
+        {
+            var servicio = await _context.Servicios.Where(x => x.CodServicio == id).FirstOrDefaultAsync();
+            if (servicio != null)
+            {
+                _context.Servicios.Remove(servicio);
+            }
+
+            return true;
         }
     }
 }
