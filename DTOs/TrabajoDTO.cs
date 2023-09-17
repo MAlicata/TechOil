02433fe152613e1ec51031136340dd8f9d23f950
Matias Alicata <matiasalicata@gmail.com>
2023-09-17 20:52:41 -0300

    Agregar operaciones CRUD en Trabajo

--- DTOs/TrabajoDTO.cs
+++ DTOs/TrabajoDTO.cs
@@ -2,5 +2,11 @@
 {
     public class TrabajoDTO
     {
+        public DateTime Fecha { get; set; }
+        public int CodProyecto { get; set; }
+        public int CodServicio { get; set; }
+        public int CantHoras { get; set; }
+        public decimal ValorHora { get; set; }
+        public decimal Costo { get; set; }
     }
 }
