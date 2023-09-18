d342886adcc864d54459414c7405af30c6f251be
Matias Alicata <matiasalicata@gmail.com>
2023-09-18 15:12:45 -0300

    Agregar PasswordRncryptHelper

---
+++ Helper/PasswordEncryptHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography; //para encriptar la password
+using System.Text;
+
+namespace TechOil.Helper
+{
+    public static class PasswordEncryptHelper
+    {
+        public static string EncryptPassword(string password)
+        {
+            var sha256 = SHA256.Create(); //creamos un algoritmo
+            var encoding = new ASCIIEncoding(); //instaciamos ASCII
+            var stream = Array.Empty<byte>();
+            var sb = new StringBuilder();
+
+            stream = sha256.ComputeHash(encoding.GetBytes(password));
+            for(int i = 0; i < stream.Length; i++)
+            {
+                sb.AppendFormat("{0:x2}", stream[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
